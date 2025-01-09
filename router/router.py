2b2a4aa39aa9cae9a74b45b698cad386bdaa0a0e
parkjusu3321 <jackpot3016@naver.com>
2025-01-09 16:11:44 +0900

    #01.09 DTO add

--- router/router.py
+++ router/router.py
@@ -3,6 +3,12 @@ from schemas.schemas import UserCreate, UserOut
 from crud.crud import create_user, get_user_by_id, verify_password
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.database import provide_session
+from pydantic import BaseModel
+
+class LoginDTO(BaseModel):
+    user_ID: str
+    user_PW: str
+
 
 user_router = APIRouter()
 
@@ -16,18 +22,20 @@ async def sign_up(user: UserCreate, db: AsyncSession = Depends(provide_session))
     return new_user
 
 
+
+user_router = APIRouter()
+
 @user_router.post("/users/login")
 async def login_user(
-    user_ID: str = Form(...),  # Form 필드로 변경
-    user_PW: str = Form(...),  # Form 필드로 변경
+    form_data: LoginDTO,  # DTO를 경로 함수에서 사용
     db: AsyncSession = Depends(provide_session)
 ):
-    db_user = await get_user_by_id(user_ID, db)
+    db_user = await get_user_by_id(form_data.user_ID, db)
 
     if db_user is None:
         raise HTTPException(status_code=400, detail="User not found.")
 
-    if not await verify_password(user_PW, db_user.password):
+    if not await verify_password(form_data.user_PW, db_user.password):
         raise HTTPException(status_code=400, detail="Password mismatch.")
 
     return {"message": "Login successful", "user": {"id": db_user.id, "name": db_user.name}}
